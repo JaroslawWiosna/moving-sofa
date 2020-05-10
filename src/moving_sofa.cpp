464e95d9f4eafa809bbcc5c84a7fd6b8d1840549
Jarosław Wiosna <Jaroslaw.Wiosna.github@gmail.com>
2020-05-10 19:58:39 +0200

    (#3) Introduce box and a rough rotation
    which is not fully working

--- src/moving_sofa.cpp
+++ src/moving_sofa.cpp
@@ -8,6 +8,40 @@
 #include "moving_sofa_10_pixel.cpp"
 #include "moving_sofa_20_image.cpp"
 #include "moving_sofa_30_vec.cpp"
+
+
+#include <cmath>
+struct Box {
+    Vec2i pos{};
+    Vec2i size{};
+    int rotation{};
+    void rotate(int angle, Vec2i point) {
+        int len = sqrt(pow(pos.x - point.x,2) + pow(pos.y - point.y,2));
+
+        // 360 == 2pi
+        // x  == y
+
+        // y = 2pi / 360 * x
+
+        pos.x += len*cos(angle * 180 / 3.1415);
+        pos.y += len*sin(angle * 180 / 3.1415);
+        rotation += angle;
+    }
+    bool contains(Vec2i point) {
+        Vec2i rotated_point {point};
+        int len = sqrt(pow(pos.x - point.x,2) + pow(pos.y - point.y,2));
+        rotated_point.x -= len*cos(-rotation * 180 / 3.1415);
+        rotated_point.y -= len*sin(-rotation * 180 / 3.1415);
+
+        if (rotated_point.x > pos.x && rotated_point.x < pos.x + size.x && 
+        rotated_point.y > pos.y && rotated_point.y < pos.y + size.y) {
+            return true;
+        }
+
+        return false;
+    }
+};
+
 #include "moving_sofa_40_sofa.cpp"
 #include "moving_sofa_50_line.cpp"
 #include "moving_sofa_60_corridor.cpp"
--- src/moving_sofa_99_main.cpp
+++ src/moving_sofa_99_main.cpp
@@ -1,4 +1,32 @@
+void test() {
+    {
+        Box box;
+        box.pos = {500, 500};
+        box.size = {100, 100};
+        box.rotate(180, {500, 500});
+        assert(box.contains({450, 550}));
+    }
+    {
+        Box box;
+        box.pos = {500, 500};
+        box.size = {200, 200};
+        box.rotate(180, {500, 500});
+        assert(box.contains({450, 550}));
+    }
+    {
+        Box box;
+        box.pos = {500, 500};
+        box.size = {100, 100};
+        box.rotate(10, {500, 500});
+        box.rotate(-10, {500, 500}); 
+        assert(box.contains({550, 550}));
+    }
+
+}
+
 int main(int argc, char* argv[]) {
+    test();
+
     Plane plane{};
     plane.render("output00.png");
 
